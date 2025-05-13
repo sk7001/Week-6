979467387339713c518847fa5ef7feee27151ae4
Srinivas Sobhit Kintali <sk7001@srmist.edu.in>
2025-05-13 16:45:42 +0530

    First commit

---
+++ 04 - Spring Framework and Multi-Layer Development with H2 DB/EmployeePayrollApp/src/main/java/com/example/employeepayrollapp/dto/EmployeeDTO.java
@@ -0,0 +1,18 @@
+package com.example.employeepayrollapp.dto;
+
+public class EmployeeDTO {
+    private String name;
+    private double salary;
+
+    public EmployeeDTO() {}
+
+    public EmployeeDTO(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getSalary() { return salary; }
+    public void setSalary(double salary) { this.salary = salary; }
+}
